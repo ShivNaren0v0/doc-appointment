4040ca1d185a98ec77ca53001bb531e75c45a36c
Thanush A  A <aathanush@gmail.com>
2024-03-21 12:32:45 +0530

    Completed the implementation of appointment dashboard
    Minor bug fixes needs to be done

--- src/app/components/view-appointments/view-appointments.component.ts
+++ src/app/components/view-appointments/view-appointments.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
-
+import { Component,inject } from '@angular/core';
+import { AppointmentwidgetComponent } from '../appointmentwidget/appointmentwidget.component';
+import { CommonModule } from '@angular/common';
+import { Appointment } from '../../interfaces/appointment';
+import { PatientService } from '../../services/patient.service';
 @Component({
   selector: 'app-view-appointments',
   standalone: true,
-  imports: [],
+  imports: [AppointmentwidgetComponent, CommonModule],
   templateUrl: './view-appointments.component.html',
   styleUrl: './view-appointments.component.css'
 })
 export class ViewAppointmentsComponent {
+
+  appointmentList: Appointment[] = [];
+  filteredAppointmentList: Appointment[] = [];
+  patientService = inject(PatientService);
+
+  constructor(){
+    this.appointmentList = this.patientService.getAllAppointments();
+    this.filteredAppointmentList = this.appointmentList;
+  }
+  filterResults(date: string) {
+    if(!date) {
+    this.filteredAppointmentList = this.appointmentList;
+  }
+    this.filteredAppointmentList.filter(
+      appointment => appointment.date.includes(date));
+
+  }
 
 }
