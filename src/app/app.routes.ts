398a50500b6eee8c789c3e1948c99159c81e06bf
Thanush A  A <aathanush@gmail.com>
2024-03-21 13:46:32 +0530

    Updated naming of login to home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
 import { ReviewComponent } from './components/review/review.component';
-import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
 import { PatientRegistrationComponent } from './components/patient-registration/patient-registration.component';
 import { DocdetailsComponent } from './components/docdetails/docdetails.component';
 import { PatientDashboardComponent } from './components/patient-dashboard/patient-dashboard.component';
@@ -8,7 +8,7 @@ import { PatientComponent } from './components/patient/patient.component';
 import { NotfoundComponent } from './components/notfound/notfound.component';
 import { ViewAppointmentsComponent } from './components/view-appointments/view-appointments.component';
 export const routes: Routes = [
-    {path: '', component: LoginComponent},
+    {path: '', component: HomeComponent},
 
     { path: 'patient', component: PatientComponent, children: [
         { path: 'patient-dashboard', component: PatientDashboardComponent },
